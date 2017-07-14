df45d2a08fd8b6b68b7fc069b142243f4527069f
mr.chen <564964001@qq.com>
2017-07-14 11:40:07 +0800

    后台移动文件方法

--- server.php
+++ server.php
@@ -17,4 +17,32 @@ foreach ($_FILES as $key => $obj){
 
 var_dump($_POST);
 
+// 服务端移动文件方法
+
+/**
+ * @param $FileObj 文件对象
+ * @param $FileSrc  新的文件路径
+ * @param $FileName 文件名称
+ * @return bool     返回结果
+ */
+function FilesMoveUploaded($FileObj, $FileSrc, $FileName){
+
+    // 判断文件路径是否存在
+    if (!file_exists($FileSrc)) {
+        //创建文件夹
+        @mkdir($FileSrc, 0777);
+    }
+    //移动文件
+    $TmpPath = $FileObj["tmp_name"];//取得上传文件。当前的临时路径
+    //
+    $SavePath = $FileSrc.$FileName;//正式文件路径和名字
+    //
+    $trace = move_uploaded_file($TmpPath, $SavePath);//将上传的临时文件目录里的文件,立即移动到新的位置进行重命名保存
+
+    // 返回执行结果
+    return $trace;
+
+}
+
+
 ?>
